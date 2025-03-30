9dc63bc7f8f7bb3b4a0aa41962054cbb7862e201
Anri <@anrigabaidze44@gmail.com>
2025-03-31 01:22:21 +0400

    fix footer layout

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -7,14 +7,9 @@ export default function Footer() {
   return (
     <footer className="Footer" id="footer">
       <div className="Footer-Container">
+        
         <div className="Footer-PersonalData">
           <p>&copy; 2025 RC. ყველა უფლება დაცულია</p>
-        </div>
-        <div className="Social-Contact-Email">
-          <p>ელ-ფოსტა</p>
-          <a href="mailto:info@rc.com.ge" id="contact-email" className="ContactEmail" target="_blank">
-            info@rc.com.ge
-          </a>
         </div>
         <div className="Social-Contact-Links">
           <p>სოციალური ქსელები</p>
@@ -27,7 +22,15 @@ export default function Footer() {
             Facebook
           </a>
         </div>
+        <div className="Social-Contact-Email">
+          <p>ელ-ფოსტა</p>
+          <a href="mailto:info@rc.com.ge" id="contact-email" className="ContactEmail" target="_blank">
+            info@rc.com.ge
+          </a>
+        </div>
+      
       </div>
+      
     </footer>
   );
 }
